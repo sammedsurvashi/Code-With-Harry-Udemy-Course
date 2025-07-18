141c797c3bcc30b0d69dcca67aef6177f8119dab
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-07-18 18:39:59 +0530

    Update syntax.py

--- syntax.py
+++ syntax.py
@@ -1,7 +1,7 @@
 print("Hello Sammed")   #Strings like "Hello Sammed" must be in double quotes ("") or single quotes ('').
-print("I am good")      # prints: I am good
-print("How are you?")   # prints: How are you
-print(3)                 #Numbers like 3 can be printed directly (no quotes needed).
+print("I am good")      #prints: I am good
+print("How are you?")   #prints: How are you
+print(3)                #Numbers like 3 can be printed directly (no quotes needed).
 
 
 #print() is a built-in function used to show output in Python.
