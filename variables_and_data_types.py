a8288b7335d2a1e8cd71c6af1bb0b6f02e8891cc
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-07-21 19:13:15 +0530

    Create variables_and_data_types.py

---
+++ variables_and_data_types.py
@@ -0,0 +1,6 @@
+age= 34 #integer
+name="sammed" #string
+cgpa=7.88 #float
+
+#In python varibles are used to  store data that can be used and manipulated throughout a program . 
+A variable is created the moment you assign a value to it using  the assignment operator ()=
