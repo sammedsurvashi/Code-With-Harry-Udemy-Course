43cf5c7abedbf4713df86813c924f9726e676a48
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-07-23 14:27:50 +0530

    Update data_types.py

--- data_types.py
+++ data_types.py
@@ -1,1 +1,36 @@
+#python  supports serveral built-in data types :--
+
+# 1 Integers (int):-- whole numbers (e.g. 10,-5)
+
+#Floats (float):-- Decimal numbers (e.g. 3.14,-0.001)
+
+#Strings (str):-- Text data enclosed in quotes (e.g."Hello",'python')
+
+#Booleans (bool) :-- Represents True or False
+
+#Lists :-- Ordered, mutable collections (e.g.{1,2,3,4}) 
+
+#Tuples :-- ordered, immtable collections (e.g.{1,2,3})
+
+#Sets :--Unordered collections of unique elements (e.g.{1,2,3})     
+
+#Dictionaries:-- key value pairs (e.g.{"name": "sammed", "age":25})  
+
+
+age = 3 
+print(age)   # e.g.int
+print(type(age))
+
+cgpa = 8.2
+print(cgpa)  # e.g.float
+print(type(cgpa))
+
+name = "sammed"
+print(name)     # e.g.str
+
+is_completed = True # can also be false
+print(is_completed)
+print(type(is_completed))
+
+
 
