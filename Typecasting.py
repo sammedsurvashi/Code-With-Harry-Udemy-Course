d3dbc16f99cfb3c4b121dbaf785fa0517dcaf079
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-07-25 15:06:08 +0530

    Update Typecasting.py

--- Typecasting.py
+++ Typecasting.py
@@ -10,3 +10,21 @@ print(type(b))
 c =int(b)
 print(c)
 print(type(c))
+
+
+#Examples
+
+ #convert string to  integer
+num_str ="10"
+num_int =(num_str)
+print(num_int) # output: 10
+
+#convert integer to string
+num = 25
+num_str = str(num)
+print(num_str) #output: "25"
+
+#convert float to integer
+pi = 3.14
+pi_int = int(pi)
+print(pi_int)  #output:3
