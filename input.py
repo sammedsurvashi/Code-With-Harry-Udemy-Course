d1a908b4869d34f898d59ee5f99f93a8fa898bce
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-07-26 15:07:33 +0530

    Update input.py

--- input.py
+++ input.py
@@ -4,3 +4,13 @@ b = int(input("Enter second number"))    #eg.26
 print(a+b)                               
                                          #output : 51
 
+
+========================================================================
+
+# 1.Take a Name as Input
+
+name = input("what is your name?")
+print("Hello,",Sammed)
+
+#output: Hello,Sammed
+
