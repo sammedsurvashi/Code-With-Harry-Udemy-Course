17ecca05ddf50425ddc3503fc7ea3b4aeb1808bd
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-07-26 15:02:38 +0530

    Create input.py

---
+++ input.py
@@ -0,0 +1,6 @@
+a = int(input("Enter first  number"))    #eg.25 
+
+b = int(input("Enter second number"))    #eg.26         
+print(a+b)                               
+                                         #output : 51
+
