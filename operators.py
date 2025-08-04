088d473798aa6164bd80e44af32e13ff20b2cadd
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-08-04 17:14:01 +0530

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,13 @@
+# 1. Arithmetic Operators
+a = 10
+b = 3
+
+print("Arithmetic Operators")
+
+print("Addition:", a + b)           # 13
+print("Subtraction:", a - b)        # 7
+print("Multiplication:", a * b)     # 30
+print("Division:", a / b)           # 3.333...
+print("Modulus:", a % b)            # 1
+print("Exponent:", a ** b)          # 1000
+print("Floor Division:", a // b)    # 3
