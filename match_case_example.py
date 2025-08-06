9f4676dd130f2f4e9577a937c6754b7761688d90
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-08-06 23:17:31 +0530

    Create match_case_example.py

---
+++ match_case_example.py
@@ -0,0 +1,14 @@
+
+day = 3
+
+match day:
+    case 1:
+        print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case _:
+        print("Invalid day")
+
+output : wednesday
