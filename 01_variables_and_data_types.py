59d1a22c05a50240b7b69868ca54ae270a8c3b6e
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-08-07 13:34:02 +0530

    Update 01_variables_and_data_types.py

--- 01_variables_and_data_types.py
+++ 01_variables_and_data_types.py
@@ -1,6 +1,33 @@
-age= 34 #integer
-name="sammed" #string
-cgpa=7.88 #float
+#python  supports serveral built-in data types :--
 
-#In python varibles are used to  store data that can be used and manipulated throughout a program . 
-A variable is created the moment you assign a value to it using  the assignment operator ()=
+# 1 Integers (int):-- whole numbers (e.g. 10,-5)
+
+# 2 Floats (float):-- Decimal numbers (e.g. 3.14,-0.001)
+
+# 3 Strings (str):-- Text data enclosed in quotes (e.g."Hello",'python')
+
+# 4 Booleans (bool) :-- Represents True or False
+
+# 5 Lists :-- Ordered, mutable collections (e.g.{1,2,3,4}) 
+
+# 6 Tuples :-- ordered, immtable collections (e.g.{1,2,3})
+
+# 7 Sets :--Unordered collections of unique elements (e.g.{1,2,3})     
+
+#8 Dictionaries:-- key value pairs (e.g.{"name": "sammed", "age":25})  
+
+
+age = 3 
+print(age)   # e.g.int
+print(type(age))
+
+cgpa = 8.2
+print(cgpa)  # e.g.float
+print(type(cgpa))
+
+name = "sammed"
+print(name)     # e.g.str
+
+is_completed = True # can also be false
+print(is_completed)
+print(type(is_completed))
