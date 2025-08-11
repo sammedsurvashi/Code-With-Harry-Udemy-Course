3e3dc027f5edd11e0670b57089cca1a9ae7c5c46
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-08-11 16:43:57 +0530

    Update 10_break_continue_pass_demo.py

--- 10_break_continue_pass_demo.py
+++ 10_break_continue_pass_demo.py
@@ -1,1 +1,16 @@
+for i in range(5):
+    if i == 2:
+        continue  # skip when i is 2
+    if i == 4:
+        break            #stop when i is 4
+    print(i)
+
+# pass example
+for i in range(3):
+    pass            # placeholder for future code
+
+output:
+0
+1
+3
 
