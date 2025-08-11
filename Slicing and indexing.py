4c816c420339c070db3108e3f83f7515b957b855
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-08-11 20:05:29 +0530

    Create Slicing and indexing.py

---
+++ Slicing and indexing.py
@@ -0,0 +1,20 @@
+#Indexing: To access each character in a string, we use an index (starts from 0, and -1 means the last character).
+
+#Slicing: To take a part of a string, we use [start:end] (the end index character is not included).
+
+text = "Python"
+
+# Indexing
+print(text[0])   # Output: P (पहला अक्षर)
+print(text[-1])  # Output: n (आखिरी अक्षर)
+
+# Slicing
+print(text[0:4])  # Output: Pyth (0 से 3 तक के अक्षर)
+print(text[2:])   # Output: thon (इंडेक्स 2 से आखिरी तक)
+print(text[:3])   # Output: Pyt (शुरुआत से 2 तक)
+
+
+#Indexes are of two types:
+
+Positive Index – Starts from 0 from the left side (beginning of the string).
+Negative Index – Starts from -1 from the right side (end of the string). 
