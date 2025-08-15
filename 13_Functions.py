db2a4a48a41b7f60d237470b895d3897bb1d5309
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-08-15 20:21:12 +0530

    Update 13_Functions.py

--- 13_Functions.py
+++ 13_Functions.py
@@ -1,1 +1,7 @@
+#Functions help in reusability and modularity in python 
 
+
+def greet(name):
+    return f"Hello,{name}!"   # returns a string with the name inserted
+
+print(greet("Alice"))  # Output: Hello, Alice!
