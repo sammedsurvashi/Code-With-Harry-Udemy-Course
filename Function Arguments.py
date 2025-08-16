0c0a93848af0f6106da504f5d94f7236d11a4a7a
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-08-16 13:27:28 +0530

    Create Function Arguments.py

---
+++ Function Arguments.py
@@ -0,0 +1,9 @@
+#Function Arguments: The values we pass to a function when calling it, so the function can use them to perform its task.
+
+
+def add(a, b):          # a and b = arguments (inputs)
+    return a + b        # returning the result (output)
+
+# Function call
+result = add(5, 3)      # here 5 and 3 are arguments
+print(result)           # Output: 8
