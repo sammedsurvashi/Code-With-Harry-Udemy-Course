73745f1215f205bbd16e1225f73fe69e1bbc9b1a
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-08-16 13:38:16 +0530

    Update 15_lambda.py

--- 15_lambda.py
+++ 15_lambda.py
@@ -1,1 +1,14 @@
 #Lambda function in Python = a small, anonymous (nameless) function written in one line using lambda
+
+
+# Normal function to add two numbers.                         # Same work using lambda
+                                                               add = lambda a, b: a + b
+                                                                print(add(5, 3))   # Output: 8
+def add(a, b):
+    return a + b
+
+print(add(5, 3))   # Output: 8
+
+
+#Normal function → uses def, can have multiple lines, has a name.
+#Lambda function → uses lambda, single line, mostly used for short/temporary work.
