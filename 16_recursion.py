6d85dd5c93e3a2126ed1646989fd868c4731158e
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-08-16 19:29:29 +0530

    Update 16_recursion.py

--- 16_recursion.py
+++ 16_recursion.py
@@ -1,1 +1,27 @@
+#“Recursion is when a function calls itself until a base case stops it.
+(#Recursion वह प्रक्रिया है जिसमें कोई function खुद को बार-बार call करता है, जब तक base case न आ जाए)
 
+#Example – Number Countdown.
+
+  def coutdown(n):
+#base case (stopping condition)
+
+if n == 0;
+print("Done!")
+return
+
+#work
+print(n)
+
+#recursion case (function calls itself)
+countdown(n - 1)
+
+countdown(5)
+
+#output : 
+5
+4
+3
+2
+1
+Done!
