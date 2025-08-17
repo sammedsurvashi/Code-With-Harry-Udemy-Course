5ff3923627ee5867ddfc14bcee908d5f94cbcb51
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-08-17 16:42:16 +0530

    Update 19_lists.py

--- 19_lists.py
+++ 19_lists.py
@@ -1,1 +1,10 @@
-lists
+#A list is a collection of items in Python.
+#Lists are ordered, mutable (changeable), and allow duplicate values.
+#Created using square brackets [ ].
+
+
+#Creating a List
+
+my_list = [10,20,30,"Hello",3.14]
+print(my_list)                           #[10,20,30,"Hello",3.14]
+print(type(my_list))                       #<class 'list'>
