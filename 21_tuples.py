aa8c626135e1735dfff51cc28749f6478b85b4cd
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-08-18 18:33:01 +0530

    Update 21_tuples.py

--- 21_tuples.py
+++ 21_tuples.py
@@ -39,5 +39,25 @@ fruits2 = ("banana", "orange")
 
 print(fruits1 + fruits2)   # ('apple', 'mango', 'banana', 'orange')
 print(fruits1 * 2)         # ('apple', 'mango', 'apple', 'mango')
+...............................................................
+
+4.#Functions (len, max, min, sum)
+#Common functions work on tuples.
+
+t = (5, 10, 15)
+
+print(len(t))   # 3   (length / number of elements)
+print(max(t))   # 15  (maximum / largest value)
+print(min(t))   # 5   (minimum / smallest value)
+print(sum(t))   # 30  (sum / total of all values)
+............................................................
+5.#Methods (count, index)
+#There are only two methods in a tuple
+
+t = (1, 2, 2, 3)
+print(t.count(2))   # 2 occurs 2 times / 2 दो बार आया
+print(t.index(3))   # index = 3 / position 3 पर है
+
+
 
 
