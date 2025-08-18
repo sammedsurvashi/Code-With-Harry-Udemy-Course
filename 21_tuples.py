d6f232fb23acd48ad97827c92698aef6d8e90199
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-08-18 17:49:41 +0530

    Update 21_tuples.py

--- 21_tuples.py
+++ 21_tuples.py
@@ -1,1 +1,11 @@
+#Tuple is similar to a list but immutable (its values cannot be changed).
 
+
+a = (3,2,22,13)
+
+print(a)
+print(a[2])  #output:22
+
+#but
+a [3] =32     #a[3] is 13
+#error: typeerror tuple object does not support item assignment(can't change)
