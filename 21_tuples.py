472a551a4db8d40dd82d376822c0239e5949e2b7
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-08-18 18:28:07 +0530

    Update 21_tuples.py

--- 21_tuples.py
+++ 21_tuples.py
@@ -12,7 +12,32 @@ a [3] =32     #a[3] is 13
 
 ..................................................
 
-#creating a tuple
+1.#creating a tuple
 
-my_tuple = (10,20,30)
+my_tuple = (10,20,30)  #Tuples are created using () (parentheses).
 single_element = (5,) #tuple with one element comma required.
+
+.......................................................
+
+  2.#Accessing Elements (Indexing – घटक काढणे
+  t = (1, 2, 3, 4)
+print(t[0])   #1 (first element)
+print(t[-1])  #4 (last element)
+#or
+t = (10, 20, 30, 40, 50)
+print(t[0])    # 10
+print(t[-1])   # 50
+print(t[1:4])  # (20, 30, 40)
+......................................................
+
+3.#*Operations (+ , )
++ = Join / Add tuples
+* = Repeat tuple elements
+
+fruits1 = ("apple", "mango")
+fruits2 = ("banana", "orange")
+
+print(fruits1 + fruits2)   # ('apple', 'mango', 'banana', 'orange')
+print(fruits1 * 2)         # ('apple', 'mango', 'apple', 'mango')
+
+
