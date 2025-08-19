30b20a95c00756cb5fabc5864ee0b578611d7087
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-08-19 17:14:14 +0530

    Update 23_dictionary_examples.py

--- 23_dictionary_examples.py
+++ 23_dictionary_examples.py
@@ -1,1 +1,13 @@
+#Dictionary is a collection of "Key-Value pairs".
+#Key = unique (duplicates are not allowed).
+#Value = can be anything (string, number, list, dictionary).
+#Dictionary is written in curly braces {}.
 
+#example:--
+student = {
+    "name": "Sammed",
+    "age": 21,
+    "course": "BCA"
+}
+print(student)
+# Output: {'name': 'Sammed', 'age': 21, 'course': 'BCA'}
