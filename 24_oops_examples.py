5720640b78f4dd173671b96fa6d124ae1c853a41
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-08-19 20:53:01 +0530

    Update 24_oops_examples.py

--- 24_oops_examples.py
+++ 24_oops_examples.py
@@ -1,1 +1,63 @@
+#Object-Oriented Programming (OOP) in Python:--
+{#OOP (Object-Oriented Programming) is a programming style where we organize code using Classes (design) and Objects (real things).
+
+#OOP म्हणजे अशी programming style जिथे आपला code आपण Class (नकाशा/डिझाईन) आणि Object (प्रत्यक्ष वस्तू) वापरून व्यवस्थित ठेवतो.}
+
+  1.Class:-Class is just a design or blueprint.
+
+# Example of Class
+class Car:   # <-- हे class आहे
+    def __init__(self, name, color):   # Constructor (class सुरू होताना चालतो)
+        self.name = name   # Attribute #(गुणधर्म)
+        self.color = color # Attribute 
+.......................................................................................
+
+  2.Object:--     
+#Object is the real thing created from the class.
+#Object म्हणजे class वरून तयार झालेली प्रत्यक्ष वस्तू.
+
+# Example of Object
+c1 = Car("BMW", "Black")   # <-- हे Object आहे
+c2 = Car("Audi", "White")  # <-- हे दुसरं Object आहे.
+.........................................................................................
+
+3.Encapsulation:-- (Putting Data + Methods Together)
+# Encapsulation means keeping data and methods together in one unit.
+ #Encapsulation म्हणजे Data आणि Methods एकत्र ठेवणे आणि काही माहिती लपवणे.
+
+
+
+
+4.Inheritance:--
+ #Inheritance means using the properties of one class into another.
+ #Inheritance म्हणजे एका class चे गुणधर्म दुसऱ्या class मध्ये वापरणे.
+
+
+
+
+
+
+5.Polymorphism:--
+#Polymorphism means one thing working in different ways.
+#Polymorphism म्हणजे एकच गोष्ट वेगवेगळ्या प्रकारे काम करणं.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Class → Blueprint / Design (नकाशा)
+#Object → Class वरून तयार झालेली वस्तू (Instance)
+#Encapsulation → Data + Methods एकत्र (Security)
+#Inheritance → एका class चे गुणधर्म दुसऱ्यात (Code reuse)
+#Polymorphism → एकच गोष्ट वेगवेगळ्या प्रकारे काम करणं
+
 
