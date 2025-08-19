59826d3a00eea6394a6dd703bb673f20eab54d82
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-08-19 17:17:17 +0530

    Update 23_dictionary_examples.py

--- 23_dictionary_examples.py
+++ 23_dictionary_examples.py
@@ -11,3 +11,22 @@ student = {
 }
 print(student)
 # Output: {'name': 'Sammed', 'age': 21, 'course': 'BCA'}
+
+..........................................................
+
+    #Types of dictionary creation:--
+
+    # Normal way
+d1 = {"a": 1, "b": 2, "c": 3}
+print(d1)  #output:{'a': 1, 'b': 2, 'c': 3}
+
+
+# Using dict() function
+d2 = dict(x=10, y=20, z=30)
+print(d2)   # {'x': 10, 'y': 20, 'z': 30}  #output:{'x': 10, 'y': 20, 'z': 30}
+
+
+# Nested dictionary
+d3 = {"person": {"name": "Sammed", "age": 21}}
+print(d3) #output:{'person': {'name': 'Sammed', 'age': 21}}
+
