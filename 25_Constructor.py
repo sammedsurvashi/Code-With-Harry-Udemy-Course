46018c35fd7118b6bca6f4563613722a5e32af67
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-08-20 16:35:32 +0530

    Update 25_Constructor.py

--- 25_Constructor.py
+++ 25_Constructor.py
@@ -1,1 +1,27 @@
+#What is a constructor?
+#A constructor is a special method.
+#In Python, it is called __init__().
+#This method runs automatically when an object is created.
+#That is, the constructor does its initial setup when an object is created.
+
+#Syntax:-
+class ClassName:
+    def __init__(self, parameters):
+        self.variable = parameters
+
+
+
+class Car:
+    # Constructor
+    def __init__(self, name, color):
+        self.name = name      # attribute (object चं नाव)
+        self.color = color    # attribute (object चा रंग)
+
+# Objects तयार झाले म्हणजे constructor आपोआप चालेल
+c1 = Car("BMW", "Black")
+c2 = Car("Audi", "White")
+
+print(c1.name, c1.color)   # BMW Black
+print(c2.name, c2.color)   # Audi White
+
 
