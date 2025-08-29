3a7f6e8cc57e420482138c0df4a932b5c6427a5c
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-08-29 17:29:57 +0530

    Update 31_getter_setter_example.py

--- 31_getter_setter_example.py
+++ 31_getter_setter_example.py
@@ -1,1 +1,6 @@
+#Getter and Setter in Python
 
+#Getter → To get a value from an object (read).
+#Setter → To update a value in an object (write) + To apply some rules..
+#Getter → object मधून value काढण्यासाठी (read).
+#Setter → object मध्ये value update करण्यासाठी (write) + काही rule लावण्यासाठी.
