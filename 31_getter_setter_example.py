497ca12bc1bdaedaad2cff77d3f22b37c89e5809
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-08-29 17:32:47 +0530

    Update 31_getter_setter_example.py

--- 31_getter_setter_example.py
+++ 31_getter_setter_example.py
@@ -4,3 +4,9 @@
 #Setter → To update a value in an object (write) + To apply some rules..
 #Getter → object मधून value काढण्यासाठी (read).
 #Setter → object मध्ये value update करण्यासाठी (write) + काही rule लावण्यासाठी.
+
+#Getter आणि Setter म्हणजे काय?
+ Getter = value घेण्यासाठी (read करायला).
+ Setter = value बदलण्यासाठी (update करायला).
+
+#Python मध्ये आपण @property (getter) आणि @<name>.setter (setter) वापरतो.
