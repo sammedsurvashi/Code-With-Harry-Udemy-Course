5c3fc8b06a641ebc6ab1c77aa5fd0b900356cfcf
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-08-29 14:21:49 +0530

    Update 30_decorators.py

--- 30_decorators.py
+++ 30_decorators.py
@@ -1,1 +1,4 @@
- 
+Python Decorators:--
+
+English: A decorator is a function that takes another function as input and adds extra features without modifying the original code.
+Hindi: Decorator एक ऐसा function है जो दूसरे function को input लेता है और extra काम जोड़ता है, बिना original code बदले. 
