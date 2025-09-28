17d5f572fe53c19e0e066a4911fc8faba410dd31
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-09-28 14:12:23 +0530

    Update 37_ArgsAndKwargs.py

--- 37_ArgsAndKwargs.py
+++ 37_ArgsAndKwargs.py
@@ -1,1 +1,15 @@
+#1) *args (arguments)
 
+#This is useful when you don't know in advance how many values ​​to pass to a function.
+
+#*args means to get multiple values ​​by putting them in a tuple.
+
+#Example:
+def add_numbers(*args):
+    print(args)  # tuple मिळते
+    return sum(args)
+
+print(add_numbers(1, 2, 3))       # 6
+print(add_numbers(10, 20, 30, 40)) # 100
+
+#The values ​​in args are in the form of a tuple: (1,2,3)
