89011ffdc80ac3dc7afebf5f047c714c650086b2
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-09-28 14:14:33 +0530

    Update 37_ArgsAndKwargs.py

--- 37_ArgsAndKwargs.py
+++ 37_ArgsAndKwargs.py
@@ -13,3 +13,33 @@ print(add_numbers(1, 2, 3))       # 6
 print(add_numbers(10, 20, 30, 40)) # 100
 
 #The values ​​in args are in the form of a tuple: (1,2,3)
+
+..............................................................................................
+
+#2) **kwargs (keyword arguments)
+
+#This is used when values ​​are to be given in the form of key-value pairs.
+
+#**kwargs means to get values ​​by storing them in a dictionary.
+
+def show_details(**kwargs):
+    print(kwargs)  # dictionary मिळते
+    for key, value in kwargs.items():
+        print(key, "=", value)
+
+show_details(name="Sammed", age=21, city="Pune")
+
+#The values ​​in kwargs are in dictionary format:
+{'name': 'Sammed', 'age': 21, 'city': 'Pune'}
+
+
+
+
+
+
+
+
+
+
+
+
