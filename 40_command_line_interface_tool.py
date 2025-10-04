4b603cb01702ea3bb948ba285535057783dd6f9f
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-10-04 15:59:22 +0530

    Update 40_command_line_interface_tool.py

--- 40_command_line_interface_tool.py
+++ 40_command_line_interface_tool.py
@@ -1,1 +1,5 @@
+ #Creating Command Line Utilities
 
+#A Command Line Utility (CLI Tool) is a small program or script that can be run from the command line or a terminal, such as Windows CMD, Linux Terminal, or Mac Terminal.
+#This means you can run a program by simply typing commands in the terminal instead of using a GUI (graphical interface).
+#These tools are used to automate small tasks.
