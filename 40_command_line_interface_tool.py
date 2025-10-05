71867040fb63543fc2abcb59ca4242ffc84776d1
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-10-05 16:18:37 +0530

    Update 40_command_line_interface_tool.py

--- 40_command_line_interface_tool.py
+++ 40_command_line_interface_tool.py
@@ -3,3 +3,11 @@
 #A Command Line Utility (CLI Tool) is a small program or script that can be run from the command line or a terminal, such as Windows CMD, Linux Terminal, or Mac Terminal.
 #This means you can run a program by simply typing commands in the terminal instead of using a GUI (graphical interface).
 #These tools are used to automate small tasks.
+
+argparse module → command line arguments handle करता है
+add_argument() → define करता है कि CLI में कौन-कौन से options चाहिए
+parse_args() → input arguments read करता है
+args.name / args.age → terminal से दिए गए values access करता है
+print() → final message display करता है
+........................................................................
+
