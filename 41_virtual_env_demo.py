3e91faac190b2cad341adab4c703bb650af37105
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-10-09 23:53:54 +0530

    Update 41_virtual_env_demo.py

--- 41_virtual_env_demo.py
+++ 41_virtual_env_demo.py
@@ -5,3 +5,14 @@ When creating projects in Python, we need different libraries (packages).
 Each project can have a different version.
 That's why Python uses Virtual Environments —
 This creates a separate space for each project where we can install our packages.
+
+2. What is a Virtual Environment?
+Definition:
+A virtual environment is an isolated workspace for Python projects.
+It allows you to install and manage packages for one project, without affecting others.
+
+Example:
+
+Project A → needs numpy 1.25
+Project B → needs numpy 1.23
+Both can work without conflict if each has its own virtual environment.
