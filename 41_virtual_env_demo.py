8a5b09e881126fd3a1fd1c3269e3f4c8d170728a
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-10-09 23:52:40 +0530

    Update 41_virtual_env_demo.py

--- 41_virtual_env_demo.py
+++ 41_virtual_env_demo.py
@@ -1,1 +1,7 @@
+#Virtual Environments & Package Management
 
+1. Introduction
+When creating projects in Python, we need different libraries (packages).
+Each project can have a different version.
+That's why Python uses Virtual Environments —
+This creates a separate space for each project where we can install our packages.
