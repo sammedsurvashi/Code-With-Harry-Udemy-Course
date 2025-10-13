0d291f179103961b82e00aed18f1aaf4e18fefbe
sammedsurvashi <sammedsurvashi26@gmail.com>
2025-10-14 01:37:38 +0530

    Update 42_requests_api_examples.py

--- 42_requests_api_examples.py
+++ 42_requests_api_examples.py
@@ -1,1 +1,24 @@
+#Requests Module - Working with APIs
 
+
+1. What is an API?
+
+An API (Application Programming Interface) is a bridge that allows two applications to share and exchange data.
+
+ Example:
+
+Weather app → makes an API call to get the temperature from a weather server.
+When you look up "weather in Pune," the app sends an API request and receives data in response. 
+
+............................................................................................
+
+2. What is the Requests Module?
+
+Python's requests module allows us to receive and send data from web APIs.
+
+We can send HTTP requests using::-
+
+GET → to read data
+POST → to send data
+PUT → to update data
+DELETE → to delete data
